c2838d3f83629d73225e72703d5e2be0c939dea3
Yan Shcherbakov <yanshcherbakov@Yans-MacBook-Pro-2.local>
2013-06-30 14:04:46 -0400

    Created ListViewController which is a UITableViewController, it creates a connection with Google.

---
+++ Google Destination Predictions/ListViewController.m
@@ -0,0 +1,13 @@
+//
+//  ListViewController.m
+//  Google Destination Predictions
+//
+//  Created by Yan Shcherbakov on 2013-06-30.
+//  Copyright (c) 2013 One Shot Apps. All rights reserved.
+//
+
+#import "ListViewController.h"
+
+@implementation ListViewController
+
+@end
